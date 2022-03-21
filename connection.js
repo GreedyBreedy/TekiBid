073a7827bfb65a0b7eb30ba2ba4372e656b0b43d
Miguel <95565345+GreedyBreedy@users.noreply.github.com>
2022-03-21 20:20:05 +0200

    Add files via upload

--- connection.js
+++ connection.js
@@ -1,11 +1,26 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/node-mongodb', {useNewUrlParser: true});
-var conn = mongoose.connection;
-conn.on('connected', function() {
-    console.log('database is connected successfully');
+const { MongoClient } = require('mongodb');
+
+
+
+const uri = "mongodb://miguelibrahim:Pagi9021@cluster0-shard-00-00.lrfwj.mongodb.net:27017,cluster0-shard-00-01.lrfwj.mongodb.net:27017,cluster0-shard-00-02.lrfwj.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-sx9b0g-shard-0&authSource=admin&retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+async function main() {
+client.connect();
+console.log("Connection was successful");
+var name = document.getElementById("name").value;
+var email = document.getElementById("email").value;
+var password = document.getElementById("password").value;
+var phoneNumber = document.getElementById("phone").value;
+createUser(client, {
+    name: name,
+    email: email,
+    password: password,
+    phone: phoneNumber,
 });
-conn.on('disconnected',function(){
-    console.log('database is disconnected successfully');
-})
-conn.on('error', console.error.bind(console, 'connection error:'));
-module.exports = conn;
+location.href = "singup_sucess.html";
+}
+main().catch(console.error);
+async function createUser(client, newUser) {
+const result = await client.db("SignUp").collection("Registration").insertOne(newUser);
+console.log(`The ID: ${result.insertedId}`);
+}
